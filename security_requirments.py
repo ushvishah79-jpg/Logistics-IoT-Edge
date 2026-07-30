dc9620af65b24212b5e9128275d9517ba28d579b
Riya kamble <riyask140907@gmail.com>
2026-07-30 19:37:41 +0530

    Add files via upload

---
+++ security_requirments.py
@@ -0,0 +1,11 @@
+requirements = [
+    "SHA-256 Verification",
+    "Digital Signature Validation",
+    "HTTPS Communication",
+    "Secure Key Storage"
+]
+
+print("Security Requirements")
+
+for req in requirements:
+    print("-", req)
